00693ea4de31e2142dc96dc53ba94a4d0f39bc53
Jared Lumpe <mjlumpe@gmail.com>
2017-07-20 16:23:30 -0700

    Cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Clean up output directories
+
+rm -rf assemblies assemblies_500/ BUSCO_stats/ PhiX_free_reads/ Plasmid_assemblies/ Prokka_stats/ trimmed_PhiX_free_reads/
+rm -rf *_assembly/ *_PLASMID_contigs

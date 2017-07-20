b9fd6fecf80189fe5fe03153d37ecd10e277ed2c
Jared Lumpe <mjlumpe@gmail.com>
2017-07-20 13:44:15 -0700

    Refactor main script, support running from another directory

--- bbduk-trimmomatic-SPAdes-plasmidSPAdes-prokka-BUSCO.bash
+++ bbduk-trimmomatic-SPAdes-plasmidSPAdes-prokka-BUSCO.bash
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 # Stop on first error
 set -e
 
+# Get directory of this script - don't necessarily want files to be in same directory as scripts
+# Should support symlinks
+# (readlink isn't POSIX - so may not be entirely portable. See
+# https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself)
+DIR=`dirname $(readlink -f $0)`
+
+# These are still hard-coded for now - change them
+BUSCO_LINEAGE=/path/to/BUSCO/lineage_data/species_odb9
+
+# Create output directories
 mkdir PhiX_free_reads
 mkdir trimmed_PhiX_free_reads
 mkdir assemblies
@@ -11,16 +21,17 @@ mkdir Plasmid_assemblies
 mkdir Prokka_stats
 mkdir BUSCO_stats
 
-perl run_bbduk.pl raw_reads/*
-
+# Run bbduk
+perl $DIR/run_bbduk.pl $DIR/phiX174.fasta raw_reads/*
 mv raw_reads/*_noPhiX PhiX_free_reads
 
-perl run_trimmomatic.pl PhiX_free_reads/*
-
+# Run trimmomatic
+perl $DIR/run_trimmomatic.pl $DIR/adapters.fasta PhiX_free_reads/*
 mv PhiX_free_reads/*paired_trimmed* trimmed_PhiX_free_reads
 
-perl run_SPAdes.pl trimmed_PhiX_free_reads/*
-perl run_plasmidSPAdes.pl trimmed_PhiX_free_reads/*
+# Run SPAdes
+perl $DIR/run_SPAdes.pl trimmed_PhiX_free_reads/*
+perl $DIR/run_plasmidSPAdes.pl trimmed_PhiX_free_reads/*
 
 #move assemblies to different folder
 for file in *_assembly/scaffolds.fasta; do new="$(echo "$file" | cut -d '_' -f 1)".scaffolds.fasta; cp "$file" "assemblies/$new"; done
@@ -36,7 +47,7 @@ for i in assemblies_500/*.fna
 do
 	location="$(echo "$i" | cut -d '.' -f 1)"
 	name="$(echo "$location" | cut -d '/' -f 2)"
-	BUSCO.py -i $i -o BUSCO_$name -l /path/to/BUSCO/lineage_data/species_odb9 -m geno -c 12
+	BUSCO.py -i $i -o BUSCO_$name -l $BUSCO_LINEAGE -m geno -c 12
 done
 
 #take all BUSCO summaries and plot data
